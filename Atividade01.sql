47a7f7196155d36780d46f413c1185083c81af02
Eduardo Emanuel Carvalho Ribeiro <70245046+edurib17@users.noreply.github.com>
2021-06-12 12:13:27 -0300

    Update Atividade01.sql

--- Atividade01.sql
+++ Atividade01.sql
@@ -1,3 +1,11 @@
+/*
+
+• Usando o banco de dados empresa.sql. Criar uma FUNCÃO que receba o ID de um produto (prod_id) e a quantidade comprada e retorner o valor do preço (prod_price) com desconto conforme as condições seguintes:
+• quantidade <= 3 desconto = 0.2
+• quantidade > 3 desconto = 0.5
+
+*/
+
 CREATE OR REPLACE FUNCTION produto_desconto(id_produto character, quantidade int) 
 RETURN float IS
 product_desconto float; 
