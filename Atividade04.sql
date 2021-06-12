e52a38073318b81a621a42497ab3ce0733b2577c
Eduardo Emanuel Carvalho Ribeiro <70245046+edurib17@users.noreply.github.com>
2021-06-12 12:14:59 -0300

    Update Atividade04.sql

--- Atividade04.sql
+++ Atividade04.sql
@@ -1,3 +1,9 @@
+/*
+Criar um PROCEDIMENTO para atualizar o preço dos produtos (products). O id e o preço chegarão por parâmetro. Em uma tabela audit_produtos registrar o id do produto, o usuário, quando a operação foi executada e qual a operação foi (armazenar a informação de deleção, 
+update e insert). Criar a tabela audit_produtos no script. Usar uma TRIGGER para essa operação de registro de log.
+
+*/
+
 CREATE OR REPLACE PROCEDURE atualizarProduto(preco_produto float,id_produto character) 
 IS
 BEGIN 
